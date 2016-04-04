2dbc6cdd8a84cec3222175b3e4f77fd8cb9191bd
atinaputri <putri.atina@gmail.com>
2016-04-04 18:45:45 +0700

    create/edit post

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,10 @@ from django.conf.urls.static import static
 from django.conf.urls import url
 from . import views
 
+
 urlpatterns = [
         url(r'^$', views.post_list, name='post_list'),
         url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
-    ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+        url(r'^post/new/$', views.post_new, name='post_new'),
+        url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
+    ]
