2dbd38ee13cd364e6649154a6169066a3297210a
atinaputri <putri.atina@gmail.com>
2016-04-04 16:13:17 +0700

    Added view+template CSS

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,9 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.conf.urls import url
 from . import views
 
 urlpatterns = [
         url(r'^$', views.post_list, name='post_list'),
-    ]
+        url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
